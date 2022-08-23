a613c89d22852b86521539b6d375fa769eb3f78b
Jaime Sanchez <jes10_22@hotmail.com>
2022-08-22 22:47:01 -0500

    Se agrega la carpeta del Spring

---
+++ movementHandler/src/main/java/com/Mh/movementHandler/MovementHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.Mh.movementHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementHandlerApplication.class, args);
+	}
+
+}
---
+++ movementHandler/src/test/java/com/Mh/movementHandler/MovementHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mh.movementHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
