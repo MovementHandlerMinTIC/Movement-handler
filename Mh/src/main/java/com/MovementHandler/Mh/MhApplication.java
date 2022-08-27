cc849b845d600f752232f7d2c852887ad1d0ef35
Gabriela Chavez <gchavez39@misena.edu.co>
2022-08-27 08:29:57 -0500

    Se ingresaron los datos a la clase MhAplication (clase main), para poder ejecutar las clases Empleado, Empresa y MovimientodeDinero

--- Mh/src/main/java/com/MovementHandler/Mh/MhApplication.java
+++ Mh/src/main/java/com/MovementHandler/Mh/MhApplication.java
@@ -1,5 +1,8 @@
 package com.MovementHandler.Mh;
 
+import com.MovementHandler.Mh.entities.Empleado;
+import com.MovementHandler.Mh.entities.Empresa;
+import com.MovementHandler.Mh.entities.MovimientodeDinero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,42 @@ public class MhApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MhApplication.class, args);
+		Empleado empleado1 = new Empleado("Juan Perez", "juanperez@gmail.com", "Coca Cola", "Operativo");
+		System.out.println("Nombre empleado: "+ empleado1.getNombreEmpleado());
+		empleado1.setNombreEmpleado("Laura Amaya");
+		System.out.println("Nombre empleado: "+ empleado1.getNombreEmpleado());
+		System.out.println("Correo empleado: "+ empleado1.getCorreoEmpleado());
+		empleado1.setCorreoEmpleado("lauramaya@gmail.com");
+		System.out.println("Correo empleado: "+ empleado1.getCorreoEmpleado());
+		System.out.println("Empresa: "+ empleado1.getEmpresaEmpleado());
+		empleado1.setEmpresaEmpleado("Coca-Cola");
+		System.out.println("Empresa: "+ empleado1.getEmpresaEmpleado());
+		System.out.println("Rol empleado: "+ empleado1.getRolEmpleado());
+		empleado1.setRolEmpleado("Administrador");
+		System.out.println("Rol empleado: "+ empleado1.getRolEmpleado());
+
+		Empresa empresa1 = new Empresa("Coca Cola", "cra. 42a #93-03", "309 4574", "10000335-4");
+		System.out.println("Nombre empresa: "+ empresa1.getNombreEmpresa());
+		empresa1.setNombreEmpresa("Coca-Cola");
+		System.out.println("Nombre empresa: "+ empresa1.getNombreEmpresa());
+		System.out.println("Dirección empresa: "+ empresa1.getDireccionEmpresa());
+		empresa1.setDireccionEmpresa("kra. 42a #93-03");
+		System.out.println("Dirección empresa: "+ empresa1.getDireccionEmpresa());
+		System.out.println("Telefono: "+ empresa1.getTelefonoEmpresa());
+		empresa1.setTelefonoEmpresa("602 309 4574");
+		System.out.println("Telefono: "+ empresa1.getTelefonoEmpresa());
+		System.out.println("NIT: "+ empresa1.getNitEmpresa());
+		empresa1.setNitEmpresa("10056794-5");
+		System.out.println("NIT: "+ empresa1.getNitEmpresa());
+
+		MovimientodeDinero mov1= new MovimientodeDinero(10.000, "Venta de cuaderno", "Carolina Rojas");
+		System.out.println("Monto del movimiento: "+ mov1.getMontoMovimiento());
+		mov1.setMontoMovimiento(-10.0);
+		System.out.println("Monto del movimiento: "+ mov1.getMontoMovimiento());
+		System.out.println("Concepto del movimiento: "+ mov1.getConceptoMovimiento());
+		mov1.setConceptoMovimiento("Venta de cuaderno especial");
+		System.out.println("Concepto del movimiento: "+ mov1.getConceptoMovimiento());
+		System.out.println("Encargado del movimiento: "+ mov1.getUsuarioEncargado());
 	}
 
 }
