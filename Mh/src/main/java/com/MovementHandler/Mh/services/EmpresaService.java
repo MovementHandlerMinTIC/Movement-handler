967c754eb0d1274962fe48308513a42ce709bca6
Daniel Hilarion Galindez <danielitohgr@gmail.com>
2022-09-07 20:43:29 -0500

    no son cambios

---
+++ Mh/src/main/java/com/MovementHandler/Mh/services/EmpresaService.java
@@ -0,0 +1,6 @@
+package com.MovementHandler.Mh.services;
+
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public class EmpresaService extends JpaRepositoriesAutoConfiguration {
+}
